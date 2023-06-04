703cc52b56f750861f57347ca29fcee78940024e
Ibsen Gomes <119980119+Ibsen-Gomes@users.noreply.github.com>
2023-06-04 19:08:07 -0300

    index add
    adição de index entre amostras e os centroides + comparação com o Sklearn.

--- codes/Kmeans.py
+++ codes/Kmeans.py
@@ -1,4 +1,7 @@
-############################################ Código K-means (machine learning) ##################################################
+###################################### Código K-means (machine learning) ########################################
+################################# Adaptado para dados reais do campo de Namorado ################################
+########################################### Ibsen P. S. Gomes ###################################################
+############################ Observatório Nacional - Universidade Federal Fluminense ############################
 
 # Bibliotecas
 import numpy as np 
@@ -8,6 +11,19 @@ import sys
 import random
 from math import sqrt
 
+
+### Normalização dos dados (pré processamento):
+
+def norm_data(data):
+    
+    data_norm = np.zeros((len(data)))
+    for i in range(len(data)):
+        data_norm[i] = (data[i] - min(data))/(max(data) - min(data))
+        
+    return data_norm
+
+
+### Métricas:
 
 # Distância Euclidiana:
 def euclidian(v1, v2, check_input=True):
@@ -32,8 +48,8 @@ def euclidian(v1, v2, check_input=True):
     
     #Armazena o quadrado da distância
     dist = 0.0
-    for x in range(len(v1)):
-        dist += (v1[x] - v2[x])**2
+    for i in range(len(v1)):
+        dist += (v1[i] - v2[i])**2
  
     #Tira a raiz quadrada da soma
     eucli = sqrt(dist)
@@ -63,15 +79,16 @@ def manhattan(v1, v2, check_input=True):
     
     #Armazena a distância simples
     dist = 0.0
-    for x in range(len(v1)):
-        dist += (v1[x] - v2[x])
+    for i in range(len(v1)):
+        dist += (v1[i] - v2[i])
  
     #atribui dist para a variável de saida manhattan
     manhattan = dist
     return manhattan
 
 
-# inicialização kmeans++:
+### Inicialização kmeans++:
+
 def kmeans_plus_plus(data, k):
     
     '''
@@ -117,51 +134,61 @@ def kmeans_plus_plus(data, k):
 
 
 # K-means:
+
 def K_means(data, k, it, tol, random='range'):
     
     '''
     Esse código aplica o método k-means a um conjunto de dados de entrada
     
     entrada:
+    
     data = dados a serem agrupados;
     k = n° de centroides (quantos grupos se quer dividir os dados)
     tol = tolerância 
     random = forma de inicialização dos centroides
     
     método:
+    
     -> calculo da distância entre cada dado com os centroides
     -> atualização da posição do centroide para a média do grupo
     -> se posição atual - posição anterios dos centroides for menor que tolerância (tol) o código para
     -> se o critério de tolerância não for atendida, o código continua a realizar as iterações (talvez faça todas as iterações!)
     
     saída:
-    bestmatches = dados separados em grupos
+
     centroids = coordenadas dos centroides após o fim das iterações 
     conta = quantas iterações foram realizadas
+    index = etiqueta de cada amostra apoós a convergência
+    inércia = soma do quadrado da distância intra-cluster
     '''
      
-    centroids = np.zeros((k,2)) # Array vazio que será preencido pelos Centroids
+    nprop = data.shape[1]
+    centroids = np.zeros((k, nprop)) # Array vazio que será preencido pelos Centroids (automatizar isso)
     
     # Condicional -> se "range", os centroides serão encolhidos de forma aleatória dentro de um range:
     if random == 'range':
-        for i in range(k):
-            x = np.array(np.random.uniform(min(data[:,0]),max(data[:,0])))
-            y = np.array(np.random.uniform(min(data[:,1]),max(data[:,1])))
-            centroids[i] = x, y
+        
+        for npro in range(nprop):
+            for i in range(k):
+                centroids[i,npro]  = np.array(np.random.uniform(min(data[:,npro]),max(data[:,npro])))
             
     # Condicional -> se "input_data", os centroides serão escolhidos entre os dados de entrada:     
     if random == 'input_data':
+        
         for i in range(k):
             index = np.random.randint(data.shape[0])
             centroids[i] = data[index,:]
             
     # Condicional -> para inicialização k-means++:
     if random == 'kmeans++':
+        
         centroids = kmeans_plus_plus(data, k) # chamando a função kmeans++
         
-    #tol = 1
+        
     conta = 0
-    lastmatches = None
+    ###lastmatches = None
+    index = [0.0]*np.size(data,0) # para armazenar as etiquetas de cada amostra
+    
     #O número de iterações será no máximo 100
     for t in range(it):
         bestmatches = [[] for i in range(k)]
@@ -172,8 +199,9 @@ def K_means(data, k, it, tol, random='range'):
             bestmatche = 0 #Aqui armazeno o índice da menor distância para comparação
             for i in range(k):
                 d = euclidian(centroids[i],row) #Calcula a distancia em relação ao centroide
-                if d < euclidian(centroids[bestmatche],row): ###Comparação entre as distâncias entre um dado e todos centroides?
+                if d < euclidian(centroids[bestmatche],row): ###Armazenando a menor distância entre a amaostra e o centroide!
                     bestmatche = i
+            index[j] = bestmatche # add um indice que representa o centroide mais próximo
             bestmatches[bestmatche].append(j)
      
         #Move o centroide para a zona média do cluster
@@ -188,12 +216,24 @@ def K_means(data, k, it, tol, random='range'):
                     avgs[j] /= len(bestmatches[i])
                 centroids[i]=avgs
                 
-        # Condicional para convergência: se a posição atual dos centroides - posição anterior
-        # for menor que uma tolerância o código para e converge!
+        # Condicional para convergência: se a posição atual dos centroides - posição anterior...
+        # ...for menor que uma tolerância o código para e converge!
         if np.allclose(centroids[i],centroids[i-1], rtol=tol) == True:
             break 
             
-        lastmatches=bestmatche
+        ###lastmatches=bestmatche
         conta += 1 # conta quantas iterações ocorreram
-        
-    return bestmatches, centroids, conta 
+    
+    
+    # Cálculo da inércia:
+    inercia = 0
+    for centro_index in range (k):
+        for amostra in range (len(index)):
+            # Se a etiqueta tiver o mesmo índice que o centroide, a operação para Ínercia será realizada:
+            if index[amostra] == centro_index: 
+                inercia += sum(abs((data[amostra] - centroids[centro_index])**2))
+                
+    centroids = np.asarray(centroids)
+    
+    return centroids, conta, index, inercia
+
